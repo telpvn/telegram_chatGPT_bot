dbf0b13faf5711f9f3911f394f89d83933528ace
telpvn <telpvn@mail.ru>
2024-08-18 19:40:46 +0400

    Error code 2

--- src/main/java/com/example/telegrambot/openai/ChatCompletionObject.java
+++ src/main/java/com/example/telegrambot/openai/ChatCompletionObject.java
@@ -3,10 +3,15 @@ package com.example.telegrambot.openai;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
 public class ChatCompletionObject {
 
     private final List<Choice> choices;
+
+    public ChatCompletionObject() {
+        choices = new ArrayList<>();
+    }
 }
